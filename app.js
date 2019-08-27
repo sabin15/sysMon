ea8667c3121a175acb3ebd560ef7b8178f2ec10f
sabin15 <sabinshrestha15@outlook.com>
2019-08-27 07:38:23 +0545

    Bootstrap Table Configured

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express'); //import package
+const app = express(); 
+
+
+var http = require('http').createServer(app);
+var io= require('socket.io')(http);
+
 const bodyParser = require('body-parser');
+
+/*---------------------------------------------------------------------------------------------------------------------------------*/
+
 
 //Routes
 const route = require('./routes/routes.js');
 
 //create express app
-const app = express(); 
+
 app.set('view engine','ejs');
 
 //parse requests of content-type -application/x-www-form-urlencoded
@@ -15,6 +24,8 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 
 app.use('/monitor',route);
+
+
 
 
 
@@ -37,15 +48,37 @@ app.get('/',(req,res) =>{
 
 });
 
+
 app.post('/post',(req,res) =>{
     var cpu = req.body.cpu;
     var memory = req.body.memory;
-    console.log('cpu: '+ cpu + ' memory'+ memory +'\n');
-    res.send('cpu: '+ cpu + ' memory'+ memory+'\n');
+    var device = req.body.device;
+    
+    //io.sockets.emit(device,'cpu: '+ cpu + ' memory '+ memory +'\n');
+    io.sockets.emit(device,req.body);
+    //console.log('cpu: '+ cpu + ' memory:'+ memory +' device:'+device +'\n');
+    console.log(req.body)
+    res.send('cpu: '+ cpu + ' memory '+ memory+'\n');
+
     
 
 });
-app.listen(3000,() => {
-    console.log("Server is listening on port 3000");
+
+/* ----------------------------------------------------------------------------------------------------------------------- */
+
+io.on('connection',function(socket){
+    console.log('a user connected');
+    socket.on('chat message',function(msg){
+        console.log('message:'+msg);
+        io.emit('chat message',msg);
+        
+    });
+    socket.on('disconnect',function(){
+        console.log('user disconnected');
+    });
+});
+
+http.listen(4000,() => {
+    console.log("Server is listening on port 4000");
 });
 
